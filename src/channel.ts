7db276b124a6e45143e29a7763e7facab019c7d4
Weilence <weilence@163.com>
2019-03-02 14:29:22 +0800

    fix error

--- src/channel.ts
+++ src/channel.ts
@@ -1,9 +1,5 @@
 import { OutputChannel, window } from "vscode";
 
 export class Channel {
-    static output: OutputChannel;
-
-    constructor() {
-        Channel.output = window.createOutputChannel("Libman");
-    }
+    static output = window.createOutputChannel("Libman");
 }
